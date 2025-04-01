68041f57c6475208b1dad681e6f4f3d70c97655e
Omotola Odusanya <odusanyaomotola64@gmail.com>
2025-04-01 08:21:40 +0100

    entire code refractored

--- app/components/partner/index.tsx
+++ app/components/partner/index.tsx
@@ -2,63 +2,44 @@
 import React from "react";
 
 const PartnersSection = () => {
+  const partners = [
+    { src: "/images/1.svg", alt: "invision", height: "10px", width: "10px" },
+    { src: "/images/2.svg", alt: "apple", height: "10px", width: "10px" },
+    { src: "/images/3.svg", alt: "google", height: "30px", width: "45px" },
+    { src: "/images/4.svg", alt: "meta", height: "20px", width: "55px" },
+    { src: "/images/5.svg", alt: "microsoft", height: "40px", width: "55px" },
+    { src: "/images/6.svg", alt: "ibm", height: "20px", width: "25px" },
+    { src: "/images/7.svg", alt: "logo", height: "10px", width: "10px" },
+  ];
+
   return (
-    <div
-      id="slide"
-      className="flex flex-col lg:flex-row lg:mt-20 mt-10 mx-auto"
-    >
-      <div className="flex flex-row">
-        <img
-          src="/images/1.svg"
-          className="mr-14 mb-10"
-          alt="invision"
-          height="10px"
-          width="10px"
-        />
-        <img
-          src="/images/2.svg"
-          className="mr-14 mb-10"
-          alt="apple"
-          height="10px"
-          width="10px"
-        />
-        <img
-          src="/images/3.svg"
-          className="mr-14 mb-10"
-          alt="google"
-          height="30px"
-          width="45px"
-        />
-        <img
-          src="/images/4.svg"
-          className="mr-14 mb-10"
-          alt="meta"
-          height="20px"
-          width="55px"
-        />
-      </div>
-      <div className="flex flex-row">
-        <img
-          src="/images/5.svg"
-          className="mr-14 mb-10"
-          alt="microsoft"
-          height="40px"
-          width="55px"
-        />
-        <img
-          src="/images/6.svg"
-          className="mr-14 mb-10"
-          alt="ibm"
-          height="20px"
-          width="25px"
-        />
-        <img
-          src="/images/7.svg"
-          className="mr-14 mb-10"
-          alt="logo"
-          height="10px"
-          width="10px"
-        />
+    <div className="overflow-hidden w-full mt-10 lg:mt-20">
+      <div id="slide" className="flex whitespace-nowrap">
+        {/* First set of partners */}
+        <div className="flex animate-slide">
+          {partners.map((partner, index) => (
+            <img
+              key={`partner-1-${index}`}
+              src={partner.src}
+              className="mr-14 mb-10 inline-block"
+              alt={partner.alt}
+              height={partner.height}
+              width={partner.width}
+            />
+          ))}
+
+          {/* Duplicate set for seamless loop */}
+          {partners.map((partner, index) => (
+            <img
+              key={`partner-2-${index}`}
+              src={partner.src}
+              className="mr-14 mb-10 inline-block"
+              alt={partner.alt}
+              height={partner.height}
+              width={partner.width}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
